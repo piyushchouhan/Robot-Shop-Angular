c2d37a0126bb03775f50ec563377a670de25cdc3
Piyush Chauhan <piyush9695@gmail.com>
2024-07-17 16:05:21 +0530

    product catalog updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CatelogComponent } from './catelog/catelog.component';
+import { SiteHeaderComponent } from './site-header/site-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    CatelogComponent
+    CatelogComponent,
+    SiteHeaderComponent
   ],
   imports: [
     BrowserModule
